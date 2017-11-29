51b5caf6b491697d49ae4af3a2d833d0a34240ef
Matteo Camanni <Matteosystem@MacBookCamanni2.station>
2017-11-29 23:12:08 +0100

    prova

--- source/Destroyer.cpp
+++ source/Destroyer.cpp
@@ -6,12 +6,12 @@
 Destroyer::Destroyer(Point start_, Point end_){
     lenght=2;
     if(abs(start_.getX()-end_.getX())==lenght-1){
-        for(int i=0; i<lenght; i++){
+        for(unsigned int i=0; i<lenght; i++){
             OccupiedPoints.push_back(new Point(start_.getX() + i, start_.getY()));  
         };
     }
     else if(abs(start_.getY()-end_.getY())==lenght-1){
-        for(int i=0; i<lenght; i++){
+        for(unsigned int i=0; i<lenght; i++){
             OccupiedPoints.push_back(new Point(start_.getX(), start_.getY() + i));  
         };
     }
