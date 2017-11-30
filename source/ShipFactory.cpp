e63de5615065d49aa27390f4ab0bdadbc3c33e4e
Matteo Camanni <Matteosystem@iPad-di-Chiara.station>
2017-11-30 11:04:41 +0100

    shipfactory corretta

--- source/ShipFactory.cpp
+++ source/ShipFactory.cpp
@@ -23,19 +23,23 @@ bool ShipFactory::create(Point pstart, Point pend, Player* currentp){
     switch(len){
         case 2:
             ship = new Destroyer(pstart,pend);
+            cout << "Creato cacciatorpediniere" << endl;
             creation=true;
         break;
         case 3:
             ship = new Cruiser(pstart,pend);
+            cout << "Creato incrociatore" << endl;
             creation=true;
         break;
         case 4:
             ship = new Battleship(pstart,pend);
             creation=true;
+            cout << "Creata nave da battaglia" << endl;
         break;
         case 5:
             ship = new Carrier(pstart,pend);
             creation=true;
+            cout << "Creata portaerei" << endl;
         break;
         default:
 
@@ -45,7 +49,17 @@ bool ShipFactory::create(Point pstart, Point pend, Player* currentp){
         currentp->fleet.push_back(ship);
         vector<Point*>::iterator iter;
         for(iter=ship->OccupiedPoints.begin() ; iter!=ship->OccupiedPoints.end(); iter++){
-            (*iter)->setShippoint(true); //non va bene perchè i punti non fanno parte della griglia
+            //(*iter)->setShippoint(true); //non va bene perchè i punti non fanno parte della griglia
+            for(int i=0;i<=currentp->size;i++){
+                for(int j=0;j<=currentp->size;j++){
+                    if(**iter==currentp->map[i][j]){
+                        currentp->map[i][j].setShippoint(true);
+                    }
+                    else{
+
+                    } 
+                }
+            }
         }
     }
     return creation;
