bd0d5c7dff88591ca64433d4907af7c548b7200f
Matteo Camanni <Matteosystem@iPad-di-Chiara.station>
2017-12-02 09:11:35 +0100

    istructions

--- main.cpp
+++ main.cpp
@@ -26,6 +26,32 @@ int main(){
     Player *p2 = new Player();
     graphic::up(5);
 
+    //ISTRUZIONI
+    cout << "     ************************* ISTRUZIONI ************************* " << endl;
+    cout << "Generali" << endl;
+    cout << "     Il gioco si svoglie in una griglia 10x10 caselle numerate 0-9." << endl;
+    cout << "     Una posizione va indicata con 2 numeri sparati da virgola (y,x)." << endl;
+    cout << "Legenda" << endl;
+    cout << "     cacciatorpediniere = nave da 2 caselle" << endl;
+    cout << "        incrociatore    = nave da 3 caselle" << endl;
+    cout << "      nave da battaglia = nave da 4 caselle" << endl;
+    cout << "         portaerei      = nave da 5 caselle" << endl;
+    cout << "            [ ]         = casella base" << endl;
+    cout << "            [-]         = casella mancata" << endl;
+    cout << "            [#]         = casella colpita" << endl;
+    cout << "            [0]         = casella occupata da una nave" << endl;
+    cout << "Fase 1: Schieramento" << endl;
+    cout << "     Ogni giocatore deve posizionare 4 cacciatorpedinieri, 3 incrociatori" << endl;
+    cout << "     2 navi da battaglia e 1 portaerei. Una volta posizionate tutte le navi" << endl;
+    cout << "     tocca all'altro giocatore." << endl;
+    cout << "Fase 2: Gioco" << endl;
+    cout << "     A turno i giocatori idicano una posizione in cui sparare ricevendo" << endl;
+    cout << "     come risposta un messaggio (colpito o mancato ed eventualmente affondato)." << endl;
+    cout << "     Quando un giocatore affonda tutte le navi avversarie è dichiarato vincitore" << endl;
+    cout << endl;
+    graphic::waitUser();
+    graphic::up(24);
+    
     //CREAZIONE DELLE FLOTTE
     p1->Deploy();
     p2->Deploy();
